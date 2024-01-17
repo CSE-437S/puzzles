d1e4cff7a0a42c8241a432419e337f5d0fdab9f3
Samuel Feng <97929571+samuelyfeng@users.noreply.github.com>
2024-01-16 19:48:01 -0600

    Update day10.py

--- the-stars-align/day10.py
+++ the-stars-align/day10.py
@@ -2,6 +2,7 @@ def read_input(file_path):
     file = open(file_path, 'r')
     return file.read()
 
+# spent too much time working on a long walk and didn't get to this one. 
 
 input_lines = read_input("day_10.txt")
 print(input_lines)
