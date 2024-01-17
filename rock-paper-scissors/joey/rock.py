89d2c7d33b25ce47996cb4cde1d4daed5998d6b2
7byte9 <121839119+7byte9@users.noreply.github.com>
2024-01-16 19:06:09 -0600

    Update rock.py

--- rock-paper-scissors/joey/rock.py
+++ rock-paper-scissors/joey/rock.py
@@ -1,4 +1,15 @@
 
-def importFile():
-    cords = open('cords.txt', 'r')
-     #https://docs.python.org/3/library/csv.html
+def main():
+    input = open('./input.txt', 'r')      #https://docs.python.org/3/library/csv.html
+    theirMoves = []
+    yourMoves = []
+    theirMove = 'A'
+    yourMove = 'A'
+    for line in input:
+        print(line)
+        theirMove = line[0]
+        yourMove = line[1]
+        theirMove[line] = theirMove
+        yourMove[line] = yourMove
+
+main()
