05e73d2b04e4f30621b5be9c19e3377c5e08eb1d
sammya24 <h.samantha@wustl.edu>
2024-01-16 19:16:53 -0600

    finished long walk

---
+++ a-long-walk/h.samantha/README.md
@@ -0,0 +1,2 @@
+Hi! For the setup for my code, just make sure to change the pathanme variable to wherever you download the .txt input. When you run my code, the result should print
+"Total is: *whatever the total is*"
---
+++ a-long-walk/h.samantha/solution.java
@@ -0,0 +1,68 @@
+import java.io.*;
+
+
+public class solution {
+
+    public static void main(String[] args) throws IOException {
+        //please replace the pathname with your input below:
+        String pathname = "/Users/sammyhong/Documents/a-long-walk/a-long-walk/h.samantha/longwalkinput.txt";
+       
+        File file = new File (pathname); 
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        int sum = 0;
+
+      while (( st = br.readLine()) != null){
+        char opp_move = st.charAt(0);
+        char my_move = st.charAt(2);
+        String result;
+
+        switch (opp_move){
+            case 'A': //opp chooses rock
+                if (my_move == 'X'){
+                    result = "draw";
+                    sum = sum + 3 + 1;
+                }
+                else if (my_move == 'Y'){
+                    result = "win";
+                    sum = sum + 6 + 2;
+                }
+                else if (my_move == 'Z'){
+                    result = "loss";
+                    sum = sum + 3;
+                }
+                break;
+            case 'B': //opp chooses paper
+                if (my_move == 'X'){
+                    result = "loss";
+                    sum = sum + 1;
+                }
+                else if (my_move == 'Y'){
+                    result = "draw";
+                    sum = sum + 3 + 2;
+                }
+                else if (my_move == 'Z'){
+                    result = "win";
+                    sum = sum + 6 + 3;
+                }
+                    break;
+            case 'C': //opp chooses scissors
+                if (my_move == 'X'){
+                    result = "win";
+                    sum = sum + 6 + 1;
+                }
+                else if (my_move == 'Y'){
+                    result = "loss";
+                    sum = sum + 2;
+                }
+                else if (my_move == 'Z'){
+                    result = "draw";
+                    sum = sum + 3 + 3;
+                }
+                break;
+        }
+
+      }
+      System.out.println("Total is: " + sum);
+    }
+}
