aae8a62b6c6b4366f8a929407c9654497b2c4a71
Ben Mueller <benjm2002@gmail.com>
2024-01-16 19:01:48 -0600

    init theStarsAlign

---
+++ the-stars-align/ben.j.mueller/solution.py
@@ -0,0 +1,17 @@
+# You can see these points of light floating in the distance, 
+# and record their position in the sky and their velocity, the relative change in position per second (your puzzle input)
+
+
+# position=<-3,  6> velocity=< 2, -1>
+
+# Each line represents one point. Positions are given as <X, Y> pairs: X represents how far left (negative) or right (positive) the point appears, 
+# while Y represents how far up (negative) or down (positive) the point appears.
+
+# KEY: At 0 seconds, each point has the position given. Each second, each point's velocity is added to its position.
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    solution()
