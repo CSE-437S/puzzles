7ac0b28de2d8403ad9f79515e930cddb29f3c390
7byte9 <121839119+7byte9@users.noreply.github.com>
2024-01-16 19:00:19 -0600

    begin 2nd proj

---
+++ a-long-walk/joey/program.py
@@ -0,0 +1,3 @@
+
+def importFile():
+    cords = open('cords.txt', 'r') #https://docs.python.org/3/library/csv.html
---
+++ rock-paper-scissors/joey/rock.py
@@ -0,0 +1,4 @@
+
+def importFile():
+    cords = open('cords.txt', 'r')
+     #https://docs.python.org/3/library/csv.html
