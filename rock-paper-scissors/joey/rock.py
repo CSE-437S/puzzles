7e5ba8df9e8603f7b20d8e1d666e40f1efc723ff
7byte9 <121839119+7byte9@users.noreply.github.com>
2024-01-16 19:18:31 -0600

    Update rock.py

--- rock-paper-scissors/joey/rock.py
+++ rock-paper-scissors/joey/rock.py
@@ -3,8 +3,8 @@ def main():
     input = open('rock-paper-scissors\joey\input.txt', 'r')      #https://docs.python.org/3/library/csv.html
     theirList = []
     yourList = []
-    theirMove = 'A'
-    yourMove = 'A'
+    theirMove = ''#BLANK CHAR
+    yourMove = ''
     for line in input:
         theirMove = line[0]
         yourMove = line[1]
@@ -14,6 +14,18 @@ def main():
 
 def counter(theirList, yourList):
     sum = 0
+    shapeScores = {
+        'X':1,
+        'Y':2,
+        'Z':3
+    }
+    '''
+    A Y
+    B X
+    C Z
+    '''
+    if theirMove == 'A':
+    
     print(sum)
 
 main()
