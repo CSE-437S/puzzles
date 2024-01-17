1e204fae6ded65381d929352059d658fbc56c573
7byte9 <121839119+7byte9@users.noreply.github.com>
2024-01-16 19:12:15 -0600

    Update rock.py

--- rock-paper-scissors/joey/rock.py
+++ rock-paper-scissors/joey/rock.py
@@ -1,15 +1,19 @@
 
 def main():
-    input = open('./input.txt', 'r')      #https://docs.python.org/3/library/csv.html
-    theirMoves = []
-    yourMoves = []
+    input = open('rock-paper-scissors\joey\input.txt', 'r')      #https://docs.python.org/3/library/csv.html
+    theirList = []
+    yourList = []
     theirMove = 'A'
     yourMove = 'A'
     for line in input:
-        print(line)
         theirMove = line[0]
         yourMove = line[1]
-        theirMove[line] = theirMove
-        yourMove[line] = yourMove
+        theirList.append(theirMove)
+        yourList.append(yourMove)
+    counter(theirList, yourList)
+
+def counter(theirList, yourList):
+    sum = 0
+    print(sum)
 
 main()
