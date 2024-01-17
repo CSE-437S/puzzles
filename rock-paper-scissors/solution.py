013ba744627e3fff0c72b9bef4a9177129dcd2cd
Hoonx <106128890+Hoonx@users.noreply.github.com>
2024-01-16 19:37:52 -0600

    :(

---
+++ rock-paper-scissors/solution.py
@@ -0,0 +1,10 @@
+# A & X = 1 | B & Y = 2 | C & Z = 3 | Loss = 0 | Draw = 3 | Win = 6
+
+
+input = open("/Users/jaehunjeong/Documents/GitHub/puzzles/rock-paper-scissors/input.txt", "r")
+content = input.read()
+sum = 0
+for i, x in enumerate(content):
+    if i % 2:
+        content[i-0]
+input.close()
